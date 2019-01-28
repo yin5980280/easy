951aa653b0bbbe77e9e192e2bd8ecffabba13e57
殷林 <yinlin_cd@keruyun.com>
2019-01-28 11:07:19 +0800

    【A】新增基础查询实体

---
+++ easy-spring-boot-tk-mybatis/src/main/java/com/vartime/easy/spring/boot/tk/mybatis/web/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * @author : yinlin
+ * @date : 2019-01-28 10:58
+ * @version : 1.0
+ * @Description :
+ * @Copyright : Copyright (c) 2018
+ * @Company : KeRuYun Technology(Beijing) Chengdu Co. Ltd.
+ * @link : com.vartime.easy.spring.boot.tk.mybatis.web.package-info
+ */
+package com.vartime.easy.spring.boot.tk.mybatis.web;
