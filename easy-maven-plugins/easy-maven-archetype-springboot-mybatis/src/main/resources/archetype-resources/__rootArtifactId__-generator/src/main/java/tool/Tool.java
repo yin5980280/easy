98fb6ae17a682764e21666a2de7ba79f74c6329b
殷林 <yinlin_cd@keruyun.com>
2019-04-19 15:30:48 +0800

    【U】新增多模块maven-archetype

---
+++ easy-maven-plugins/easy-maven-archetype-springboot-mybatis/src/main/resources/archetype-resources/__rootArtifactId__-generator/src/main/java/tool/Tool.java
@@ -0,0 +1,16 @@
+package ${package}.generator.tool;
+
+import java.io.File;
+
+import cn.org.easysite.generator.mybatis.tool.GeneratorTools;
+
+/**
+ * Hello world!
+ *
+ */
+public class Tool {
+
+    public static void main(String[] args) throws Exception {
+        GeneratorTools.generator(new File(Tool.class.getClassLoader().getResource("generator/generatorConfig.xml").getFile()));
+    }
+}
