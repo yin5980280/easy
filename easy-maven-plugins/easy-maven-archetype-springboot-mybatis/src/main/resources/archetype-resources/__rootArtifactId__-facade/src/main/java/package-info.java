cec4065df62c7b849c2ba3e445a6484c218b0e3a
殷林 <yinlin_cd@keruyun.com>
2020-06-09 18:46:45 +0800

    【U】更新自动生成代码结构maven骨架

---
+++ easy-maven-plugins/easy-maven-archetype-springboot-mybatis/src/main/resources/archetype-resources/__rootArtifactId__-facade/src/main/java/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * @author : yinlin
+ * @date : 2019-04-19 15:35
+ * @version : 1.0
+ * @Description :
+ * @Copyright : Copyright (c) 2018
+ * @Company : EasySite Technology Chengdu Co. Ltd.
+ * @link : com.snb.package-info
+ */
+package ${package};
